210581dbb2b7bf24a127959a8bd7e7795cac3c07
Tên Của Bạn <vuphong28102009@gmail.com>
2025-11-14 05:07:38 +0700

    🎉 Initial commit – thêm toàn bộ dự án Lịch Sử Bác Hồ

--- frontend/src/page/Home/Home.jsx
+++ frontend/src/page/Home/Home.jsx
@@ -2,19 +2,19 @@ import { useState } from "react";
 import "./Home.css";
 
 import bia from "../../assets/home/bia-trang-chu.png";
-// import anh1 from "../../assets/home/anh1.jpg";
-// import anh2 from "../../assets/home/anh2.JPG";
-// import anh3 from "../../assets/home/anh3.JPG";
-// import anh4 from "../../assets/home/anh4.jpg";
-// import anh5 from "../../assets/home/anh5.jpg";
-// import anh6 from "../../assets/home/anh6.jpg";
+import anh1 from "../../assets/home/anh1.jpg";
+import anh2 from "../../assets/home/anh2.JPG";
+import anh3 from "../../assets/home/anh3.JPG";
+import anh4 from "../../assets/home/anh4.jpg";
+import anh5 from "../../assets/home/anh5.jpg";
+import anh6 from "../../assets/home/anh6.jpg";
 import bgSlideshow from "../../assets/home/bia-trang-chu.png"; // ảnh nền chung cho vùng about
 import aboutUs from "../../assets/home/nen.jpg";
 import why from "../../assets/home/vs.jpg"
 import thanks from "../../assets/home/lco.jpg"
 
 export default function Home() {
-    const slideshowImages = [anh1, anh2, anh3, anh4, anh5, anh6];
+    const slideshowImages = [anh1, anh2, anh3, anh5, anh6];
     const [current, setCurrent] = useState(0);
 
     const nextImage = () => {
